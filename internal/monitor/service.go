9feca9eb9190f86144c4c00e33c556c8a4b6bd8f
Jon Koenig <jkoenig@outlook.com>
2024-07-02 19:08:37 -0600

    add service methods for getting active monitors and starting monitors

--- internal/monitor/service.go
+++ internal/monitor/service.go
@@ -1,5 +1,9 @@
 package monitor
 
+import (
+	"context"
+)
+
 // NewMonitorService returns a new `MonitorService`.
 func NewMonitorService(repository MonitorRepository, distributor chan<- any) MonitorService {
 	return MonitorService{
@@ -12,4 +16,21 @@ func NewMonitorService(repository MonitorRepository, distributor chan<- any) Mon
 type MonitorService struct {
 	distributor chan<- any
 	Repository  MonitorRepository
+}
+
+func (s MonitorService) GetActive(ctx context.Context) ([]MonitorJSON, error) {
+	active := true
+	params := SelectParams{
+		Id:     nil,
+		Active: &active,
+		Kind:   nil,
+	}
+	resume, err := s.Repository.SelectMonitor(ctx, &params, 0)
+	return resume, err
+}
+
+func (s MonitorService) StartMonitor(monitor Monitor) {
+	s.distributor <- StartMessage{
+		Monitor: monitor,
+	}
 }
