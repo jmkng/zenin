ddceb94efb4551046deaf4426bdefe51393b9d49
Jon Koenig <jkoenig@outlook.com>
2024-07-13 16:05:32 -0600

    add AssertEqual

--- internal/debug/assert.go
+++ internal/debug/assert.go
@@ -6,13 +6,22 @@ import (
 )
 
 func AssertNil(t *testing.T, value any) {
+	t.Helper()
 	if value != nil {
 		t.Errorf("value is not nil: %v", value)
 	}
 }
 
+func AssertEqual[T comparable](t *testing.T, a, b T) {
+	t.Helper()
+	if a != b {
+		t.Errorf("values are not equal: %v != %v", a, b)
+	}
+}
+
 func AssertDeepEqual(t *testing.T, a, b any) {
+	t.Helper()
 	if !reflect.DeepEqual(a, b) {
-		t.Errorf("hashed values are not the same: (%v) != (%v)", a, b)
+		t.Errorf("values are not equal: %v != %v", a, b)
 	}
 }
