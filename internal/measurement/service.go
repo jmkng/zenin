ebd78d41706ee0a2d48713bca378ae427b409c21
Jon Koenig <jkoenig@outlook.com>
2024-07-13 00:49:43 -0600

    make repository public

--- internal/measurement/service.go
+++ internal/measurement/service.go
@@ -3,11 +3,11 @@ package measurement
 // NewMeasurementService returns a new `MeasurementService`
 func NewMeasurementService(repository MeasurementRepository) MeasurementService {
 	return MeasurementService{
-		repository: repository,
+		Repository: repository,
 	}
 }
 
 // MeasurementService is a service used to interact with the measurement domain type.
 type MeasurementService struct {
-	repository MeasurementRepository
+	Repository MeasurementRepository
 }
