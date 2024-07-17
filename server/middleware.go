48864083d6c4c3ba873b0e9378c7c1d32f72a615
Jon Koenig <jkoenig@outlook.com>
2024-07-17 09:47:00 -0600

    middleware `Insecure` should allow PATCH method

--- server/middleware.go
+++ server/middleware.go
@@ -51,7 +51,7 @@ func Authenticator(next http.Handler) http.Handler {
 func Insecure(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Preflight
