0e1c56c6c3329d8c24d14db012afe9397a5e1f24
Jon Koenig <jkoenig@outlook.com>
2024-08-06 18:53:46 -0600

    delete `getVisible`, move function into function

--- web/src/routes/Dashboard/Dashboard.tsx
+++ web/src/routes/Dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import { useAccountContext } from '../../internal/account';
-import { ACTIVE_UI, INACTIVE_UI, isMonitor, Monitor, useMonitorContext } from '../../internal/monitor';
+import * as monitor from "../../internal/monitor";
+import { ACTIVE_UI, INACTIVE_UI, isMonitor, useMonitorContext } from '../../internal/monitor';
 import { useDefaultMonitorService } from '../../internal/monitor/service';
 import { DataPacket } from '../../server';
 
-import EditorComponent from '../../components/Editor/Editor';
-import InfoComponent from '../../components/Info/Info';
-import MonitorComponent from '../../components/Monitor/Monitor';
+import Editor from '../../components/Editor/Editor';
+import Info from '../../components/Info/Info';
+import Monitor from '../../components/Monitor/Monitor';
 
 import './Dashboard.css';
 
@@ -15,20 +16,23 @@ export default function Dashboard() {
         service: useDefaultMonitorService()
     }
     const account = useAccountContext();
-    const sorted = getVisible(monitor.context.state.monitors, monitor.context.state.filter).sort((a, b) => a.name > b.name ? 1 : -1);
+    let sorted = [...monitor.context.state.monitors.values()];
+    if (monitor.context.state.filter == ACTIVE_UI) sorted = sorted.filter(n => n.active)
+    else if (monitor.context.state.filter == INACTIVE_UI) sorted = sorted.filter(n => !n.active)
+    sorted.sort((a, b) => a.name > b.name ? 1 : -1);
     const split = monitor.context.state.split.isEditing() || monitor.context.state.split.isViewing();
 
-    const handleAdd = async (value: Monitor) => {
+    const handleAdd = async (value: monitor.Monitor) => {
         const token = account.state.authenticated!.token.raw;
         const extract = await monitor.service.addMonitor(token, value);
         if (!extract.ok()) return;
         const body: DataPacket<{ id: number }> = await extract.json();
         const measurements = null;
-        const full: Monitor = { ...value, id: body.data.id, measurements: measurements }
+        const full: monitor.Monitor = { ...value, id: body.data.id, measurements: measurements }
         monitor.context.dispatch({ type: 'overwrite', monitor: full })
     }
 
-    const handleUpdate = async (value: Monitor) => {
+    const handleUpdate = async (value: monitor.Monitor) => {
         if (!value.id) {
             throw new Error("monitor is missing id in dashboard update");
         }
@@ -42,13 +46,13 @@ export default function Dashboard() {
         <div className={["zenin__dashboard", split ? 'split' : ''].join(' ')}>
             <div className="zenin__dashboard_monitors">
                 {sorted.map((n, i) =>
-                    <MonitorComponent key={i} monitor={n} service={monitor.service} />)}
+                    <Monitor key={i} monitor={n} service={monitor.service} />)}
             </div>
 
             {split ?
                 <div className={"zenin__dashboard_activity"}>
                     {monitor.context.state.split.isEditing() ?
-                        <EditorComponent
+                        <Editor
                             state={monitor.context.state.split.pane}
                             onClose={() => monitor.context.dispatch({ type: 'edit', monitor: null })}
                             onChange={value => isMonitor(value) ? handleUpdate(value) : handleAdd(value)}
@@ -56,18 +60,11 @@ export default function Dashboard() {
                         : null}
 
                     {monitor.context.state.split.isViewing() ?
-                        <InfoComponent
+                        <Info
                             state={monitor.context.state.split.pane} />
                         : null}
                 </div>
                 : null}
         </div>
     )
-}
-
-const getVisible = (monitors: Map<number, Monitor>, filter: string): Monitor[] => {
-    let filtered = [...monitors.values()];
-    if (filter == ACTIVE_UI) filtered = filtered.filter(n => n.active)
-    else if (filter == INACTIVE_UI) filtered = filtered.filter(n => !n.active)
-    return filtered;
 }
