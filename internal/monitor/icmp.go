7cdd9c76ea78f551338b6760700b03bfd7f81c90
Jon Koenig <jkoenig@outlook.com>
2024-08-18 13:30:19 -0600

    use context for timeout in icmp probe

--- internal/monitor/icmp.go
+++ internal/monitor/icmp.go
@@ -1,6 +1,8 @@
 package monitor
 
 import (
+	"context"
+	"errors"
 	"time"
 
 	"github.com/jmkng/zenin/internal/measurement"
@@ -15,65 +17,57 @@ func NewICMPProbe() ICMPProbe {
 type ICMPProbe struct{}
 
 // Poll implements `Probe.Poll` for `ICMPProbe`.
-func (i ICMPProbe) Poll(monitor Monitor) measurement.Span {
+func (i ICMPProbe) Poll(m Monitor) measurement.Span {
 	span := measurement.NewSpan(measurement.Ok)
 
-	client := icmp.New(*monitor.RemoteAddress)
+	client := icmp.New(*m.RemoteAddress)
 	err := client.Resolve()
 	if err != nil {
 		span.Downgrade(measurement.Dead, RemoteAddressInvalidMessage)
 		return span
 	}
 
-	// TODO: Root is needed to sent true ICMP packets. This uses UDP right now.
-	// Decide when to bind a raw socket.
-	// Should this detect root somehow, or instead rely on an environment variable, or something else?
+	// ICMP/UDP
+	if *m.ICMPProtocol == ICMP {
+		client.SetPrivileged(true)
+	}
 
-	client.Size = *monitor.ICMPSize
-	client.Interval = time.Millisecond * time.Duration(*monitor.ICMPWait)
-	client.Count = *monitor.ICMPCount
-	client.TTL = *monitor.ICMPTTL
+	client.Size = *m.ICMPSize
+	client.Interval = time.Millisecond * time.Duration(*m.ICMPWait)
+	client.Count = *m.ICMPCount
+	client.TTL = *m.ICMPTTL
 
-	timeout := time.Duration(monitor.Timeout) * time.Second
-	done := make(chan error, 1)
+	deadline, cancel := m.Deadline(context.Background())
+	defer cancel()
 
-	// TODO: Find better solution.
-	// A context can be passed in with `RunWithContext`, but pro-bing doesn't seem to return a specific error for timeouts,
-	// which makes adding a timeout hint difficult.
-	// On second thought, maybe handle timeouts in the `Poll` method so no probe has to worry about it.
-	go func(out chan<- error) {
-		err = client.Run()
-		out <- err
-	}(done)
-
-	select {
-	case err := <-done:
-		if err != nil {
-			span.Downgrade(measurement.Dead)
-			return span
+	err = client.RunWithContext(deadline)
+	if err != nil {
+		span.Downgrade(measurement.Dead)
+		if errors.Is(err, context.DeadlineExceeded) {
+			span.Hint(TimeoutMessage)
+		} else if *m.ICMPProtocol == ICMP {
+			span.Hint("Ensure Zenin has root privilege.")
 		}
-
-		stats := client.Statistics()
-		out := stats.PacketsSent
-		in := stats.PacketsRecv
-		min := stats.MinRtt.Seconds() * 1000
-		avg := stats.AvgRtt.Seconds() * 1000
-		max := stats.MaxRtt.Seconds() * 1000
-		span.ICMPPacketsOut = &out
-		span.ICMPPacketsIn = &in
-		span.ICMPMinRTT = &min
-		span.ICMPAvgRTT = &avg
-		span.ICMPMaxRTT = &max
-
-		if stats.PacketLoss > 0 {
-			// TODO: Allow setting packet loss threshold?
-			span.Downgrade(measurement.Dead, "Packet loss was detected.")
-			return span
-		}
-
-		return span
-	case <-time.After(timeout):
-		span.Downgrade(measurement.Dead, TimeoutMessage)
 		return span
 	}
+
+	stats := client.Statistics()
+	out := stats.PacketsSent
+	in := stats.PacketsRecv
+	min := stats.MinRtt.Seconds() * 1000
+	avg := stats.AvgRtt.Seconds() * 1000
+	max := stats.MaxRtt.Seconds() * 1000
+	span.ICMPPacketsOut = &out
+	span.ICMPPacketsIn = &in
+	span.ICMPMinRTT = &min
+	span.ICMPAvgRTT = &avg
+	span.ICMPMaxRTT = &max
+
+	if stats.PacketLoss > 0 {
+		// TODO: Allow setting packet loss threshold?
+		span.Downgrade(measurement.Dead, "Packet loss was detected.")
+		return span
+	}
+
+	return span
 }
