4fa706446ad9c634996c5ac3cbaa110d9f522fa1
Jon Koenig <jkoenig@outlook.com>
2024-08-18 20:17:13 -0600

    support displaying hint based on state

--- web/src/components/Input/ToggleInput/ToggleInput.tsx
+++ web/src/components/Input/ToggleInput/ToggleInput.tsx
@@ -5,15 +5,17 @@ interface ToggleProps {
     name: string;
     value: boolean;
     label?: string | React.ReactNode;
+    onSubtext?: string | React.ReactNode;
+    offSubtext?: string | React.ReactNode;
 
     onChange: (checked: boolean) => void;
 }
 
 export default function ToggleInput(props: ToggleProps) {
-    const { name, label, value, onChange } = props;
+    const { name, value, label, onSubtext, offSubtext, onChange } = props;
 
-    return (
-        <div className="zenin__toggle_input">
+    return <div className="zenin__toggle_input">
+        <div className="zenin__toggle_input_controls">
             {label ? (
                 <label
                     className="zenin__toggle_input_label zenin__input_label"
@@ -35,5 +37,12 @@ export default function ToggleInput(props: ToggleProps) {
                 />
             </div>
         </div>
-    );
+
+        {onSubtext || offSubtext
+            ? <div className="zenin__toggle_input_subtext">
+                {value && onSubtext ? onSubtext : null}
+                {!value && offSubtext ? offSubtext : null}
+            </div>
+            : null}
+    </div>
 }
