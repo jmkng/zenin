3217d2f7a60bbfff14f9c0c75dc9da6595a12b06
Jon Koenig <jkoenig@outlook.com>
2024-09-14 19:16:10 -0600

    inline logic to grab widget text

--- web/src/components/Monitor/Widget/KindWidget.tsx
+++ web/src/components/Monitor/Widget/KindWidget.tsx
@@ -1,5 +1,5 @@
-import { kindAPItoUI } from "../../../internal/monitor";
-
+import { HTTP_UI, ICMP_UI, PLUGIN_UI, TCP_UI } from "../../../internal/monitor";
+import { HTTP_API, ICMP_API, PLUGIN_API, TCP_API } from "../../../server";
 import "./KindWidget.css";
 
 interface KindWidgetProps {
@@ -9,11 +9,26 @@ interface KindWidgetProps {
 export default function KindWidget(props: KindWidgetProps) {
     const { kind } = props;
 
-    const text = kindAPItoUI(kind);
+    let text;
+    switch (kind) {
+        case HTTP_API:
+            text = HTTP_UI;
+            break;
+        case ICMP_API:
+            text = ICMP_UI;
+            break;
+        case TCP_API:
+            text = TCP_UI;
+            break;
+        case PLUGIN_API:
+            text = PLUGIN_UI;
+            break;
+        default:
+            text = "";
+            break;
+    }
 
-    return (
-        <div className="zenin__widget zenin__h_center zenin__kind_widget">
-            {text}
-        </div >
-    )
+    return <div className="zenin__widget zenin__h_center zenin__kind_widget">
+        {text}
+    </div >
 }
