dd6cba8fdd4b0fc70db91b393eec9adea048e012
Jon Koenig <jkoenig@outlook.com>
2024-10-05 13:15:22 -0600

    add `MoveIcon`

---
+++ web/src/components/Icon/MoveIcon.tsx
@@ -0,0 +1,17 @@
+export default function MoveIcon() {
+    return <svg
+        className="zenin__move_icon zenin__icon"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        style={{
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 2,
+            fill: "var(--foreground)"
+        }}
+        viewBox="0 0 24 24"
+    >
+        <path d="M16.837 18.327A1.837 1.837 0 1 1 16.835 22a1.837 1.837 0 0 1 .002-3.674Zm0-8.164a1.837 1.837 0 1 1-.002 3.675 1.837 1.837 0 0 1 .002-3.675Zm1.836-6.326A1.837 1.837 0 1 0 15 3.838a1.837 1.837 0 0 0 3.674-.001ZM7.163 18.327A1.837 1.837 0 1 1 7.162 22a1.837 1.837 0 0 1 .001-3.674Zm0-8.164a1.837 1.837 0 1 1-.001 3.675 1.837 1.837 0 0 1 .001-3.675ZM9 3.837a1.837 1.837 0 1 0-3.675.001A1.837 1.837 0 0 0 9 3.837Z" />
+    </svg>
+}
