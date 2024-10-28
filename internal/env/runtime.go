37c39f5060b24b05d34b03b0f18895edf4bb2bc5
Jon Koenig <jkoenig@outlook.com>
2024-10-28 02:23:44 -0600

    add Health method

--- internal/env/runtime.go
+++ internal/env/runtime.go
@@ -5,13 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strconv"
-	"strings"
-
-	"github.com/jmkng/zenin/internal"
 )
 
 type RuntimeKind string
@@ -19,6 +17,13 @@ type RuntimeKind string
 const (
 	Dev  RuntimeKind = "dev"
 	Prod RuntimeKind = "prod"
+)
+
+var (
+	// Version contains the program version. Assigned by linker.
+	Version string
+	// Commit contains the hash of the current commit. Assigned by linker.
+	Commit string
 )
 
 type Secret []byte
@@ -46,7 +51,7 @@ func GetRandomBytes(length int) ([]byte, error) {
 
 func NewRuntimeEnv() *RuntimeEnv {
 	kind := Prod
-	if strings.ToLower(os.Getenv(rtKindKey)) == "dev" {
+	if os.Getenv(rtKindKey) == "dev" {
 		kind = Dev
 	}
 
@@ -60,6 +65,11 @@ func NewRuntimeEnv() *RuntimeEnv {
 	redirectPort, err := strconv.ParseUint(os.Getenv(rtRedirectKey), 10, 16)
 	if err == nil {
 		redirect = uint16(redirectPort)
+	}
+
+	color := false
+	if os.Getenv(rtColorKey) == "true" {
+		color = true
 	}
 
 	signSecret, err := getSignSecret()
@@ -93,10 +103,11 @@ func NewRuntimeEnv() *RuntimeEnv {
 	return &RuntimeEnv{
 		Kind:       kind,
 		Port:       port,
+		Color:      color,
 		Redirect:   redirect,
 		SignSecret: signSecret,
 		BaseDir:    baseDir,
-		PluginDir:  pluginsDir,
+		PluginsDir: pluginsDir,
 	}
 }
 
@@ -112,42 +123,61 @@ type RuntimeEnv struct {
 	// a key will be generated at runtime.
 	SignSecret Secret
 	// BaseDir is a directory used to store files accessible to Zenin.
+	// May be an empty string if no directory can be resolved.
 	BaseDir string
-	// PluginDir is a directory used to store executable plugins.
-	PluginDir string
+	// PluginsDir is a directory used to store executable plugins.
+	// May be an empty string if no directory can be resolved.
+	PluginsDir string
+	// Version tag. May be `DEV` when running in development.
+	Version string
+	// Color determines if ANSI color sequences are included in output and logs.
+	Color bool
 }
 
-var EnvironmentError = errors.New("the environment is invalid")
-
-// Validate will return an error of type `Validation` describing problems with the runtime environment.
-func (r RuntimeEnv) Validate() internal.Validation {
-	validation := internal.NewValidation()
-
+// Health uses the provided `Diagnostic` to check for problems with the `RuntimeEnv`.
+func (r RuntimeEnv) Health(d *Diagnostic) {
 	if r.Kind == Prod && len(r.SignSecret) < 16 {
-		validation.Push("sign secret must be >= 16 bytes\nprovide a longer secret or unset `ZENIN_RT_SIGN_SECRET` to have one generated")
+		d.Warn("sign secret is weak, expected >= 16 bytes")
 	}
 
-	base, err := os.Stat(r.BaseDir)
-	if err != nil || r.BaseDir == "" {
-		validation.Push("base directory path is not set\nprovide an absolute path by setting `ZENIN_RT_BASE_DIR`")
-	} else if !base.IsDir() {
-		validation.Push("base directory path points to a file\nmake sure `ZENIN_RT_BASE_DIR` points to a directory")
+	if r.BaseDir == "" {
+		d.Error("cannot find base directory, provide a path with the `ZENIN_RT_BASE_DIR` environment variable")
+	} else {
+		base, err := os.Stat(r.BaseDir)
+		if err != nil || !base.IsDir() {
+			fmt.Println("a")
+			if errors.Is(err, fs.ErrNotExist) {
+				if err := os.MkdirAll(r.BaseDir, 0755); err != nil {
+					d.Error(fmt.Sprintf("make sure base directory exists and is accessible: `%v`", r.BaseDir))
+				}
+			} else {
+				d.Error(fmt.Sprintf("unable to access base directory: `%v`", r.BaseDir))
+			}
+		}
 	}
-	plugins, err := os.Stat(r.PluginDir)
-	if err != nil || r.PluginDir == "" {
-		validation.Push("plugins directory path is not set\nprovide an absolute path by setting `ZENIN_RT_PLUGINS_DIR`")
-	} else if !plugins.IsDir() {
-		validation.Push("plugins directory path points to a file\nmake sure `ZENIN_RT_PLUGINS_DIR` points to a directory")
+
+	if r.PluginsDir == "" {
+		d.Error("cannot find plugins directory, provide a path with the `ZENIN_RT_PLUGINS_DIR` environment variable")
+	} else {
+		plugins, err := os.Stat(r.PluginsDir)
+		if err != nil || !plugins.IsDir() {
+			if errors.Is(err, fs.ErrNotExist) {
+				if err := os.MkdirAll(r.PluginsDir, 0755); err != nil {
+					d.Error(fmt.Sprintf("make sure plugins directory exists and is accessible: `%v`", r.PluginsDir))
+				}
+			} else {
+				d.Error(fmt.Sprintf("unable to access plugins directory: `%v`", r.PluginsDir))
+			}
+		}
 	}
 
 	switch runtime.GOOS {
 	case "darwin", "linux":
 		if _, exists := os.LookupEnv("SHELL"); !exists {
-			validation.Push("must set `SHELL` to execute plugins")
+			d.Error("must set `SHELL` environment variable to execute plugins")
 		}
+		// On Windows, PowerShell is assumed.
 	}
-
-	return validation
 }
 
 func getSignSecret() (Secret, error) {
@@ -165,6 +195,7 @@ func getSignSecret() (Secret, error) {
 const (
 	rtKindKey       = "ZENIN_RT_LEVEL"
 	rtPortKey       = "ZENIN_RT_PORT"
+	rtColorKey      = "ZENIN_RT_COLOR"
 	rtRedirectKey   = "ZENIN_RT_REDIRECT"
 	rtSignSecretKey = "ZENIN_RT_SIGN_SECRET"
 	rtBaseDir       = "ZENIN_RT_BASE_DIR"
