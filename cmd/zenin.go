d53525d1a94f234e66df1d1842e1b703deb8086c
Jon Koenig <jkoenig@outlook.com>
2024-10-28 02:02:53 -0600

    print environment info, use diagnostic

--- cmd/zenin.go
+++ cmd/zenin.go
@@ -4,75 +4,82 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/jmkng/zenin/internal"
-	"github.com/jmkng/zenin/internal/bundle"
+	g "github.com/jmkng/zenin/pkg/graphics"
+
 	"github.com/jmkng/zenin/internal/env"
-	"github.com/jmkng/zenin/internal/log"
+	"github.com/jmkng/zenin/internal/meta"
 	"github.com/jmkng/zenin/internal/monitor"
+	"github.com/jmkng/zenin/internal/service"
 	"github.com/jmkng/zenin/repository"
+	"github.com/jmkng/zenin/repository/mock"
 	"github.com/jmkng/zenin/server"
 )
 
 func main() {
-	log.Debug("main starting")
 	if env.Runtime.Kind == env.Dev {
-		log.EnableDebug()
+		env.EnableDebug()
+	}
+	env.Debug("main starting")
+
+	commit := ""
+	if env.Runtime.Kind == env.Dev {
+		commit = " " + env.Commit
 	}
 
-	fmt.Println(os.Args[0])
-	os.Exit(0)
+	c := env.Runtime.Color
+	fmt.Printf("Zenin %v%v\n", env.Version, g.MagentaC(commit, c))
+	fmt.Printf("%v Environment\n", g.BrightBlackC(">", c))
+	fmt.Printf("Base: %v\n", env.Runtime.BaseDir)
+	fmt.Printf("Plugins: %v\n", env.Runtime.PluginsDir)
 
-	// Check environment.
-	dd(validate()) // TODO: Pretty print these?
+	diag := env.NewDiagnostic()
+	env.Runtime.Health(&diag)
+	if diag.Report() {
+		os.Exit(1)
+	}
+
+	meta := meta.NewMetaService(mock.NewMockRepository())
+	plugins, err := meta.GetPlugins()
+	dd(err)
+
+	fmt.Printf("Using %v plugins\n", g.BrightBlackC(fmt.Sprintf("%v", len(plugins)), c))
+	for _, v := range plugins {
+		fmt.Printf("    [%v]\n", g.BrightBlackC(v, c))
+	}
 
 	repository, err := repository.
 		Builder(env.Database).
-		WithValidate(). // TODO: Bundle this up in validate step above?
+		WithValidate().
 		Build()
 	dd(err)
 
-	bundle := bundle.NewBundle(repository)
-
-	// List plugins.
-	plugins, err := bundle.Meta.GetPlugins()
-	dd(err)
-	files := fmt.Sprintf("[%v]", strings.Join(plugins, ","))
-	log.Info("plugins", "path", env.Runtime.PluginDir, "count", len(plugins), "files", files)
+	bundle := service.NewBundle(repository)
 
 	// Resume polling active monitors.
 	active, err := bundle.Monitor.GetActive(context.Background())
 	dd(err)
-	log.Debug("resuming active monitors", "count", len(active))
+
+	env.Debug("resuming active monitors", "count", len(active))
 	for _, v := range active {
 		bundle.Monitor.Distributor <- monitor.StartMessage{Monitor: v}
 	}
 
 	// 🌩️ ->
+	fmt.Printf("%v Server\n", g.BrightBlackC(">", c))
 	err = server.
 		NewServer(server.NewConfiguration(env.Runtime), bundle).
 		Serve()
 	dd(err)
 
-	log.Debug("main stopping")
+	env.Debug("main stopping")
 }
 
-// dd will receive an error, and if the error is not nil, log it and exit with status code 1.
+// dd will log an error and exit, or do nothing if err == nil.
 func dd(err error) {
 	if err == nil {
 		return
 	}
-	log.Error("fatal error", "error", err)
+	fmt.Println(err.Error())
 	os.Exit(1)
-}
-
-// validate will run all validations, returning all messages bundled in a single `Validation`.
-func validate() internal.Validation {
-	v := internal.NewValidation()
-
-	// 1. Runtime
-	v.Join(env.Runtime.Validate())
-
-	return v
 }
