e67604cc075d0f2a9b22733c9e3ea995d601a15f
Jon Koenig <jkoenig@outlook.com>
2025-03-13 10:14:38 -0600

    refactor scripts and workflows

--- scripts/export_postgres.sh
+++ scripts/export_postgres.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
+export ZENIN_SCRIPT_REPO_CONTAINER="zenin-postgres"
 export ZENIN_REPO_KIND="postgres"
 export ZENIN_REPO_NAME="postgres"
+export ZENIN_REPO_ADDRESS="0.0.0.0"
+export ZENIN_REPO_PORT="5432"
+export ZENIN_REPO_USERNAME="username"
+export ZENIN_REPO_PASSWORD="password"
