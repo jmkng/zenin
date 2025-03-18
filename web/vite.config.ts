07796976c760cf658416ad64923f90ceef0967c7
Jon Koenig <jkoenig@outlook.com>
2025-03-18 16:45:21 -0600

    use proxy for api routes, move files to public

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,13 +5,22 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 const config = defineConfig({
     build: {
-        outDir: "../server/build"
+        outDir: "../server/build",
+        assetsDir: ".",
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: `http://localhost:${process.env.ZENIN_PORT}`,
+                changeOrigin: true,
+                secure: false
+            }
+        }
     },
     plugins: [react()],
     resolve: {
         alias: {
             "@": resolve('./src'),
-            "@fonts": resolve('./assets/fonts'),
         }
     }
 });
