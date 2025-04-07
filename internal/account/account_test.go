ea3e01c02a1537105289fea08b73961b20f0b9a6
Jon Koenig <jkoenig@outlook.com>
2025-04-06 21:52:45 -0600

    rename account validation test, add a few more test cases

--- internal/account/account_test.go
+++ internal/account/account_test.go
@@ -2,17 +2,62 @@ package account
 
 import (
 	"testing"
-
-	"github.com/jmkng/zenin/internal/debug"
 )
 
-func TestAccountValidate(t *testing.T) {
-	debug.Assert(t, CreateApplication{
-		Username:          "helloworld",
-		PasswordPlainText: "password1",
-	}.Validate() != nil, "password should be rejected")
-	debug.Assert(t, CreateApplication{
-		Username:          "helloworld",
-		PasswordPlainText: "Passw0rd1",
-	}.Validate() == nil, "password should be accepted")
+func TestIsValidAccountPassword(t *testing.T) {
+	tests := []struct {
+		password string
+		expected bool
+		name     string
+	}{
+		{
+			password: "Password123",
+			expected: true,
+			name:     "TestValidPassword",
+		},
+		{
+			password: "P@ssw0rd!",
+			expected: true,
+			name:     "TestValidWithSpecial",
+		},
+		{
+			password: "Valid1CaseUpper",
+			expected: true,
+			name:     "TestValidMixed",
+		},
+		{
+			password: "Short1!",
+			expected: false,
+			name:     "TestInvalidTooShort",
+		},
+		{
+			password: "aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!aA123!",
+			expected: false,
+			name:     "TestInvalidTooLong",
+		},
+		{
+			password: "12345678",
+			expected: false,
+			name:     "TestInvalidNoAlpha",
+		},
+		{
+			password: "password123",
+			expected: false,
+			name:     "TestInvalidNoUppercase",
+		},
+		{
+			password: "PASSWORD123",
+			expected: false,
+			name:     "TestInvalidNoLowercase",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := isValidAccountPassword(test.password)
+			if result != test.expected {
+				t.Errorf("for password '%s', expected %v but got %v", test.password, test.expected, result)
+			}
+		})
+	}
 }
